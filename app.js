c5f249e14c3ab3633a2068dae13769fa71ad3f8b
Derek Mullin <dmullin@faculty.ocadu.ca>
2014-03-28 16:19:27 -0400

    Initial.

---
+++ app.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var GIFFeed = require('GIFFeed');
+var GIF = require('./gif.js');
+
+var routes = require('./routes');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
+
+app.get('/random', routes.random);
+
+app.use(
+    function(req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    }
+);
+
+if(app.get('env') === 'development') {
+    app.use(
+        function(err, req, res, next) {
+            res.render(
+                'error',
+                {   message: err.message,
+                    error: err
+                }
+            );
+        }
+    );
+}
+
+app.use(
+    function(err, req, res, next) {
+        res.render('error',
+            {   message: err.message,
+                error: {}
+            }
+        );
+    }
+);
+
+var feed = new GIFFeed({ 'seen' : GIF.knownGIFs() });
+feed.on("GIF", GIF.handleGIF);
+feed.load();
+setInterval(feed.load, 1800000);
+
+module.exports = app;
---
+++ gif.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+
+var GIF = function() {
+
+	this.handleGIF = function(gif) {
+		fs.writeFile("public/" + gif.id + ".gif", gif.gif, function(err) {});
+		delete gif.gif;
+		fs.writeFile("metadata/" + gif.id + ".json", JSON.stringify(gif), function(err) {});
+	}
+
+	this.knownGIFs = function() {
+		var filenames = fs.readdirSync("metadata");
+		filenames.forEach(
+			function(item, index, arr) {
+				arr[index] = item.replace(".json", "");
+			}
+		);
+		return filenames;
+	}
+
+	this.randomGIF = function() {
+		var ids = this.knownGIFs();
+		var id = ids[Math.floor(Math.random()*ids.length)];
+		var gif = JSON.parse(fs.readFileSync("metadata/" + id + ".json"));
+		gif.filename = id + ".gif";
+		return gif;
+	}
+
+}
+
+module.exports = new GIF();

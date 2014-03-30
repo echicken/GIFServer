b8b5f9e7233a40742fbc815cd5627043d2f1d1eb
echicken <echicken@chickenbook.(none)>
2014-03-29 23:20:40 -0400

    Stuff

---
+++ routes.js
@@ -0,0 +1,8 @@
+var GIF = require('./gif.js');
+
+exports.random = function(request, response) {
+	var gif = GIF.randomGIF();
+	if(gif.duration < 3000)
+		gif.duration = gif.duration * 2;
+	response.render('gif', { gif: gif });
+}
